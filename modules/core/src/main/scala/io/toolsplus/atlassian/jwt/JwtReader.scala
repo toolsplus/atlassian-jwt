c4ea2c87bd5fac4bdd0dfbd5eddc561d8b580eda
Tobias Binna <tobi.binna@gmail.com>
2017-08-14 19:24:28 +0800

    Improvements based on Codacy report - Remove return statements from JwtReader.scala - Use Option checks instead of null checks in JwtReader.scala - Refactor standard claim validation by extracting individual validation logic - Remove null default parameter in Error.scala

--- modules/core/src/main/scala/io/toolsplus/atlassian/jwt/JwtReader.scala
+++ modules/core/src/main/scala/io/toolsplus/atlassian/jwt/JwtReader.scala
@@ -2,6 +2,7 @@ package io.toolsplus.atlassian.jwt
 
 import java.time.Instant
 
+import cats.syntax.either._
 import com.nimbusds.jose.crypto.MACVerifier
 import com.nimbusds.jose.{JWSObject, JWSVerifier}
 import com.nimbusds.jwt.JWTClaimsSet
@@ -32,7 +33,7 @@ case class JwtReader(sharedSecret: String) {
       case Right(jwsObject) =>
         if (shouldVerifySignature) {
           verifySignature(jwsObject) match {
-            case Right(_) => verifyRest(jwsObject, queryStringHash)
+            case Right(_)    => verifyRest(jwsObject, queryStringHash)
             case l @ Left(_) => l.asInstanceOf[Either[Error, Jwt]]
           }
         } else {
@@ -59,41 +60,89 @@ case class JwtReader(sharedSecret: String) {
     }
 
   private def verifyStandardClaims(
-      claims: JWTClaimsSet): Either[Error, JWTClaimsSet] = {
-    if (claims.getIssueTime == null || claims.getExpirationTime == null) {
-      return Left(JwtInvalidClaimError(
+      claims: JWTClaimsSet): Either[Error, JWTClaimsSet] =
+    for {
+      _ <- validateHasIssueTimeAndExpirationTime(claims)
+      now = Instant.now()
+      _ <- validateExpirationTimeIsAfterNotBefore(claims)
+      _ <- validateNowIsAfterNotBefore(now, claims)
+      _ <- validateNowIsBeforeExpirationTime(now, claims)
+    } yield claims
+
+  /** Validate that claim set contains issue time and expiration time.
+    *
+    * @param claims Claim set to validate
+    * @return Either given claim set if successful or JwtInvalidClaimError
+    */
+  private def validateHasIssueTimeAndExpirationTime(
+      claims: JWTClaimsSet): Either[Error, JWTClaimsSet] =
+    if (Option(claims.getIssueTime).isEmpty || Option(claims.getExpirationTime).isEmpty) {
+      Left(JwtInvalidClaimError(
         "'exp' and 'iat' are required claims. Atlassian JWT does not allow JWTs with unlimited lifetimes."))
+    } else {
+      Right(claims)
     }
 
-    val now = Instant.now()
-    val nowMinusLeeway =
-      now.minusSeconds(JwtReader.TIME_CLAIM_LEEWAY_SECONDS)
+  /** Validate that claim set expiration time is after 'not before' (nbf) time.
+    *
+    * Note that if 'not before' claim is not defined validation is successful.
+    *
+    * @param claims Claim set to validate
+    * @return Either given claim set if successful or JwtInvalidClaimError
+    */
+  private def validateExpirationTimeIsAfterNotBefore(
+      claims: JWTClaimsSet): Either[Error, JWTClaimsSet] =
+    if (Option(claims.getNotBeforeTime).isDefined && !claims.getExpirationTime
+          .after(claims.getNotBeforeTime)) {
+      Left(JwtInvalidClaimError(
+        s"The expiration time must be after the not-before time but exp=${claims.getExpirationTime} and nbf=${claims.getNotBeforeTime}"))
+    } else {
+      Right(claims)
+    }
+
+  /** Validate that claim set 'not before' time is before current time.
+    *
+    * Note that if 'not before' claim is not defined validation is successful.
+    *
+    * @param now Current time
+    * @param claims Claim set to validate
+    * @return Either given claim set if successful or JwtTooEarlyError
+    */
+  private def validateNowIsAfterNotBefore(
+      now: Instant,
+      claims: JWTClaimsSet): Either[Error, JWTClaimsSet] = {
     val nowPlusLeeway =
       now.plusSeconds(JwtReader.TIME_CLAIM_LEEWAY_SECONDS)
-
-    if (null != claims.getNotBeforeTime) {
-      if (!claims.getExpirationTime.after(claims.getNotBeforeTime)) {
-        return Left(JwtInvalidClaimError(
-          s"The expiration time must be after the not-before time but exp=${claims.getExpirationTime} and nbf=${claims.getNotBeforeTime}"))
-      }
-
-      if (claims.getNotBeforeTime.toInstant.isAfter(nowPlusLeeway)) {
-        return Left(
-          JwtTooEarlyError(claims.getNotBeforeTime.toInstant,
-                           now,
-                           JwtReader.TIME_CLAIM_LEEWAY_SECONDS))
-      }
+    if (Option(claims.getNotBeforeTime).isDefined && claims.getNotBeforeTime.toInstant
+          .isAfter(nowPlusLeeway)) {
+      Left(
+        JwtTooEarlyError(claims.getNotBeforeTime.toInstant,
+                         now,
+                         JwtReader.TIME_CLAIM_LEEWAY_SECONDS))
+    } else {
+      Right(claims)
     }
+  }
 
+  /** Validate that claim set expiration time is after current time.
+    *
+    * @param now Current time
+    * @param claims Claim set to validate
+    * @return Either given claim set if successful or JwtExpiredError
+    */
+  private def validateNowIsBeforeExpirationTime(
+      now: Instant,
+      claims: JWTClaimsSet): Either[Error, JWTClaimsSet] = {
+    val nowMinusLeeway =
+      now.minusSeconds(JwtReader.TIME_CLAIM_LEEWAY_SECONDS)
     if (claims.getExpirationTime.toInstant.isBefore(nowMinusLeeway)) {
-      return Left(
+      Left(
         JwtExpiredError(claims.getExpirationTime.toInstant,
                         now,
                         JwtReader.TIME_CLAIM_LEEWAY_SECONDS))
+    } else {
+      Right(claims)
     }
-
-    Right(claims)
-
   }
 
   /** Verify query string hash claim if it is present, otherwise assume
